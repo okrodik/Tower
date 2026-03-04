8eaf7cf91213ede5800c45df1fae2859f33a9423
poguk12 <okrodik@gmail.com>
2026-03-04 11:06:24 +0900

    Добавьте файлы проекта.

---
+++ Tower/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tower
+{
+    public partial class Form1 : Form
+    {
+        Panel panel1 = new Panel();
+        PictureBox pictureHero = new PictureBox();
+        PictureBox pictureEnemy = new PictureBox();
+
+        int SizeHeroEnemy = 200;
+
+        RichTextBox richTextBox = new RichTextBox();
+
+        Timer timerHero = new Timer();
+        Timer timerEnemy = new Timer();
+
+        Button btnLut1 = new Button();
+        Button btnLut2 = new Button();
+        Button btnLut3 = new Button();
+
+        public Form1()
+        {
+            InitializeComponent();
+            WindowsSetting();
+        }
+
+        private void WindowsSetting()
+        {
+            panel1.BackColor = Color.AliceBlue;
+            this.Controls.Add(panel1);
+            panel1.Dock = DockStyle.Fill;
+
+            pictureEnemy.BackColor = Color.Blue;
+            pictureEnemy.Location = new Point(Right - SizeHeroEnemy, 0);
+            pictureEnemy.Size = new Size(SizeHeroEnemy, 1000);
+
+            pictureHero.BackColor = Color.Red;
+            pictureHero.Location = new Point(Left, 0);
+            pictureHero.Size = new Size(SizeHeroEnemy, 1000);
+
+            richTextBox.BackColor = Color.LemonChiffon;
+            richTextBox.Size = new Size(150, 200);
+            richTextBox.Location = new Point(this.Width / 2 - richTextBox.Width / 2, 10);
+
+            btnLut1.Location = new Point((this.Width / 2 - btnLut1.Width / 2) - 130, (this.Height / 2) + 100);
+            btnLut2.Location = new Point(this.Width / 2 - btnLut2.Width / 2, (this.Height / 2) + 100);
+            btnLut3.Location = new Point((this.Width / 2 - btnLut3.Width / 2) + 130, (this.Height / 2) + 100);
+
+            btnLut1.Size = new Size(80, 80);
+            btnLut2.Size = new Size(80, 80);
+            btnLut3.Size = new Size(80, 80);
+
+            panel1.Controls.Add(pictureHero);
+            panel1.Controls.Add(pictureEnemy);
+            panel1.Controls.Add(richTextBox);
+
+            panel1.Controls.Add(btnLut1);
+            panel1.Controls.Add(btnLut2);
+            panel1.Controls.Add(btnLut3);
+        }
+    }
+}
