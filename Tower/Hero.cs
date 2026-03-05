0c5ead340d863b4872f12167240d50afc3fd9d72
poguk12 <okrodik@gmail.com>
2026-03-05 15:01:09 +0900

    додделаить

--- Tower/Hero.cs
+++ Tower/Hero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,21 @@ namespace Tower
         // Уровень героя
         public int Level { get; private set; }
 
+        public Hero(string name = "name", int damage = 5, int armor = 0, int health = 25, float attackSpeed = 1.0f, float critDamage = 1.1f, int critChance = 5, int evasion = 5)
+        {
+            this.Name = name;
+            this.Damage = damage;
+            this.Armor = armor;
+            this.Health = health;
+            this.AttackSpeed = attackSpeed;
+            this.CritDamage = critDamage;
+            this.CritChance = critChance;
+            this.Evasion = evasion;
+        }
+    }
+
+    class Weapon : Hero()
+    {
 
     }
 }
